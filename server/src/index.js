5477c39062ccc6f50d27757448ee47d58a661ccd
Jeff Uhrick <jjuhric@gmail.com>
2024-02-19 21:38:30 -0500

    removing .env files

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import {UserRouter} from './routes/users.js';
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth", UserRouter);
-
-mongoose.connect("mongodb+srv://jjuhric:Il0v3N1c0l3@recipe.ouafkr7.mongodb.net/recipes?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGODB_URI)
 
 app.listen(3001, () => {console.log("Server is running on port 3001")});
